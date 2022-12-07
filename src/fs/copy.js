211b3d0a70ca7a8a178cffd557a0995db9d14567
SlavikusVOG <slavaolish@gmail.com>
2022-12-08 00:50:42 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+import url from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = url.fileURLToPath(import.meta.url);
+    const __dirname = path.join(path.dirname(__filename));
+    const folder = path.join(__dirname, 'files');
+    const newFolder = path.join(__dirname, 'files_copy');
+    await fsPromises.mkdir(newFolder, { recursive: true });
+    const files = await fsPromises.readdir(folder);
+    files.forEach((file) => {
+        try{
+            fsPromises.copyFile(path.join(folder, file), path.join(newFolder, file), fsPromises.constants.COPYFILE_EXCL);
+        }
+        catch(error) {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await copy();
