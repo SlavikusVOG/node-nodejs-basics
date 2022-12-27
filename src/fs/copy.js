10d6c55dc9fc35c7da3291c90f66137d5671f719
Slava Olishevko <volishevko@xbsoftware.by>
2022-12-27 03:40:41 +0300

    feat: implement create

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,8 +1,9 @@
-import fsPromises from 'fs/promises';
-import path from 'path';
-import url from 'url';
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
 
 const copy = async () => {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import.meta#getting_current_modules_file_path
     const __filename = url.fileURLToPath(import.meta.url);
     const __dirname = path.join(path.dirname(__filename));
     const folder = path.join(__dirname, 'files');
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,25 @@
+import fsPromise from "node:fs/promises"
+import path from "node:path";
+import url from "node:url";
+
 const create = async () => {
-    // Write your code here 
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import.meta#getting_current_modules_file_path
+    const currentModule = url.fileURLToPath(import.meta.url);
+    console.log(currentModule);
+    const currentDir = path.dirname(`${currentModule}`);
+    const destDir = path.join(currentDir, 'files');
+    console.log(destDir);
+    const FILE_NAME = "fresh.txt";
+    try {
+        await fsPromise.writeFile(
+            path.join(destDir, FILE_NAME),
+            "I am fresh and young",
+            {mode: fsPromise.constants.COPYFILE_EXCL}
+        );
+    }
+    catch(err) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await create();
