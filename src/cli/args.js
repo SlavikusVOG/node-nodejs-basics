aeb7b725fd34e46c5478e65a1e4744ad025f4c6a
SlavikusVOG <slavaolish@gmail.com>
2023-01-01 02:33:31 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
     // Write your code here 
+    const cliArgs = process.argv.slice(2);
+    let result = '';
+    cliArgs.forEach((arg) => {
+        if (arg.includes('--')) {
+            result += arg.slice(2);
+        }
+        else {
+            result += ` ${arg},`;
+        }
+    });
+    result[result.length - 1] = '.';
+    console.log(result)
 };
 
 parseArgs();
