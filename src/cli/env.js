4746596c566dcfd51a8369ac97ea047c8a3681f9
SlavikusVOG <slavaolish@gmail.com>
2023-01-03 00:09:19 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
     // Write your code here 
+    const variables = process.env;
+    const envKeys = Object.keys(variables);
+    let result = '';
+    envKeys.forEach((envKey) => {
+        if (envKey.includes('RSS_')){
+            result += `${envKey}=${variables[envKey]}; `;
+        }
+    });
+    if (result.length > 0) {
+        result = result.slice(0, result.length - 2);
+    }
+    console.log(result);
 };
 
 parseEnv();
