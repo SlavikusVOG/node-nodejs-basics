561c9bb52338979a63a7101b763dd5d3c9eaa98f
SlavikusVOG <slavaolish@gmail.com>
2023-01-04 00:26:51 +0300

    feat: cp.js dev in progress

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,8 @@
+const child_process = require('node:child_process');
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    const subProcess = child_process.fork ('node files/script.js', [...args]);
 };
 
 // Put your arguments in function call to test this functionality
