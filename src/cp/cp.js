18d655899d3228d000d88bb18fb843a81bb3d676
SlavikusVOG <slavaolish@gmail.com>
2023-01-15 14:09:19 +0300

    feat: dev in progress

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,8 +1,15 @@
-const child_process = require('node:child_process');
+import child_process from 'node:child_process';
+import path from "node:path";
 
 const spawnChildProcess = async (args) => {
     // Write your code here
-    const subProcess = child_process.fork ('node files/script.js', [...args]);
+    // const subProcess = child_process.fork ('node files/script.js', [...args]);
+    const subProcessArgs = args ? [...args] : [];
+    const subProcess = child_process.spawn('node files/script.js', subProcessArgs, {stdio: ['pipe', 'pipe', process.stderr]});
+
+    subProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
