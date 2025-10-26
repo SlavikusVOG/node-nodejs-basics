789e0db96f2382e5ac19699fa184c43e340bad19
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 01:23:39 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import fs from "node:fs/promises";
+
+const errorMessage = "FS operation failed";
+const directory = "files";
+
 const list = async () => {
   // Write your code here
+  const directoryStat = await fs.stat(directory);
+  if (directoryStat.isDirectory()) {
+    const files = await fs.readdir(directory);
+    await Promise.all(
+      files.map(async (f) => {
+        const fStat = await fs.stat(`${directory}/${f}`);
+        if (fStat.isFile()) {
+          console.log(f);
+        }
+      })
+    );
+  }
+  else {
+    throw new Error (errorMessage);
+  }
 };
 
 await list();
