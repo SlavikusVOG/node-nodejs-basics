ee2c6d5caf05705f78034b9c08989abcacd81229
SlavikusVOG <slavaolish@gmail.com>
2025-10-26 23:43:30 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
+const fresh = "./files/fresh.txt";
+
 const create = async () => {
   // Write your code here
+  try {
+    await fs.access(fresh);
+    throw Error("FS operation failed");
+  }
+  catch(error) {
+    if (error.syscall === "access") {
+      try {
+        await fs.access(path.dirname(fresh));
+      }
+      catch(error) {
+        fs.mkdir(path.dirname(fresh));
+      }
+      finally{
+        const str = "I am fresh and young";
+        await fs.writeFile("files/fresh.txt", str);
+      }
+    }
+    else {
+      throw error;
+    }
+  }
 };
 
 await create();
