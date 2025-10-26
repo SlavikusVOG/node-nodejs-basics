868b321740026af14a36f7a5c91acdc7115905a8
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 00:09:24 +0300

    feat: implement copy functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import fs from "node:fs/promises";
+import path from "path";
+
+const folder = "./files";
+
 const copy = async () => {
   // Write your code here
+  try {
+    await fs.access(folder);
+    await fs.access(`${folder}_copy`);
+    throw new Error("FS operation failed");
+  }
+  catch(error) {
+    if (error.syscall === "access") {
+      try {
+        await fs.access(folder);
+        fs.cp(folder, `${folder}_copy`, {recursive: true});
+      }
+      catch(error) {
+        throw new Error("FS operation failed");
+      }
+    }
+    else {
+      throw error;
+    }
+  }
 };
 
 await copy();
