38a7de398f78de4c007e21485d06a0c4089206e6
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 01:49:30 +0300

    feat: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
   // Write your code here
+  const envVariablesKeys = Object.keys(process.env);
+  envVariablesKeys.forEach((k) => {
+    if (k.startsWith("RSS_")) {
+      console.log(`${k}=${process.env[k]}`);
+    }
+  });
 };
 
 parseEnv();
