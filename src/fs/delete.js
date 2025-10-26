65f3060821d43f40a13dd2c991abdb328e68842c
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 00:26:30 +0300

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import fs from "node:fs/promises";
+
+const errorMessage = "FS operation failed";
+const fileName = "fileToRemove.txt";
+
 const remove = async () => {
   // Write your code here
+  try {
+    await fs.access(fileName);
+    await fs.rm(fileName);
+  }
+  catch(error) {
+    if (error.syscall === "access") {
+      throw new Error(errorMessage);
+    }
+    else {
+      console.error(error);
+    }
+  }
 };
 
 await remove();
