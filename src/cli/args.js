6d3a95df22b91ffc256cd3da580aa5fcae28331e
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 11:30:51 +0300

    feat: implement args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
   // Write your code here
+  let valueNameFlag = false;
+  process.argv.reduce((previous, current) => {
+    if (previous) {
+      console.log(`${previous} is ${current}`);
+      return null
+    }
+    else if (current.startsWith("--")) {
+      return current.substr(2);
+    }
+  });
 };
 
 parseArgs();
