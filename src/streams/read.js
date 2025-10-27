4a6df38281e38c4d0002feced842c9eb36072269
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 13:55:27 +0300

    feat: implement stream:read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from "node:fs";
+
+const filePath = "./src/streams/files/fileToRead.txt";
+
 const read = async () => {
   // Write your code here
+  const input = fs.createReadStream(filePath);
+  for await (const chunk of input) {
+    process.stdout.write(chunk);
+  }
+  process.stdout.write('\n');
 };
 
 await read();
