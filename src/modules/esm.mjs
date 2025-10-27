917493659efea01ef45220769b010f81830a3b94
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 12:01:33 +0300

    feat: create ecmascript module

---
+++ src/modules/esm.mjs
@@ -0,0 +1,36 @@
+import path from 'node:path';
+import { release, version } from 'node:os';
+import { createServer as createServerHttp } from 'node:http';
+
+import ('./files/c.cjs');
+import * as a from './files/a.json' with { type: 'json' };
+import * as b from './files/b.json' with { type: 'json' };
+
+const random = Math.random();
+
+const unknownObject = random > 0.5 ? a : b;
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${import.meta.filename}`);
+console.log(`Path to current directory is ${import.meta.dirname}`);
+
+const myServer = createServerHttp((_, res) => {
+  res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
+});
+
+export default {
+  unknownObject,
+  myServer,
+};
