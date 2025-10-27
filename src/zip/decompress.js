5df37b31e264ae8f42c6b34207191ae143af88b1
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 16:47:17 +0300

    feat: implement zlib.decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import stream from "node:stream/promises";
+import fs from "node:fs";
+import zlib from "node:zlib";
+
+const filePath = "./src/zip/files/decompressed.txt";
+const archivePath = "./src/zip/files/archive.gz";
+
 const decompress = async () => {
   // Write your code here
+  await stream.pipeline(
+    fs.createReadStream(archivePath),
+    zlib.createGunzip(),
+    fs.createWriteStream(filePath),
+  )
 };
 
 await decompress();
