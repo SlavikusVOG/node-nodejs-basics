7b1e7aef4a8cfa9ad8d88714ee1252da2158e2fe
SlavikusVOG <slavaolish@gmail.com>
2025-10-27 16:21:20 +0300

    feat: implement stream:transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import stream from "node:stream";
+
 const transform = async () => {
   // Write your code here
+  const reverser = new stream.Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split('').reverse().join(''));
+    }
+  });
+  process.stdin
+    .pipe(reverser)
+    .pipe(process.stdout);
 };
 
 await transform();
