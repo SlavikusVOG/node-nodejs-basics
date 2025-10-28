383c358d2524e12810ffce8a5253ce1ea5dee377
SlavikusVOG <slavaolish@gmail.com>
2025-10-28 17:22:43 +0300

    fix: fix result output

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,13 @@
 const parseEnv = () => {
   // Write your code here
   const envVariablesKeys = Object.keys(process.env);
+  const result = []
   envVariablesKeys.forEach((k) => {
     if (k.startsWith("RSS_")) {
-      console.log(`${k}=${process.env[k]}`);
+      result.push(`${k}=${process.env[k]}`);
     }
   });
+  console.log(result.join("; "))
 };
 
 parseEnv();
